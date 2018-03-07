15f2ece0d44cfae4a6ccf9f53972b275b990e7b3
Hoai Nguyen <hnguyen@safetrust.com>
2018-03-07 12:37:45 +0700

    Init app.

---
+++ FeedbackSystemApp/app/src/main/java/android/mobile/feedbacksystem/ui/LauncherActivity.java
@@ -0,0 +1,30 @@
+package android.mobile.feedbacksystem.ui;
+
+import android.content.Intent;
+import android.mobile.feedbacksystem.MainActivity;
+import android.mobile.feedbacksystem.R;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class LauncherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                launchHomePage();
+            }
+        }, 2000);
+    }
+
+    private void launchHomePage() {
+        Intent intent = new Intent(LauncherActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
