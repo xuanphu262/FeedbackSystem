78102bb1ac13c8327dfdb836d2c557ed3c84ef47
Hoai Nguyen <hnguyen@safetrust.com>
2018-03-07 19:37:06 +0700

    Add report and setup page.

--- FeedbackSystemApp/app/src/main/java/android/mobile/feedbacksystem/MainActivity.java
+++ FeedbackSystemApp/app/src/main/java/android/mobile/feedbacksystem/MainActivity.java
@@ -1,13 +1,37 @@
 package android.mobile.feedbacksystem;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.mobile.feedbacksystem.ui.report.ReportActivity;
+import android.mobile.feedbacksystem.ui.setup.SetupActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_report).setOnClickListener(this);
+        findViewById(R.id.btn_setup).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_report:
+                Intent reportIntent = new Intent(MainActivity.this, ReportActivity.class);
+                startActivity(reportIntent);
+                break;
+            case R.id.btn_setup:
+                Intent setUpIntent = new Intent(MainActivity.this, SetupActivity.class);
+                startActivity(setUpIntent);
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
     }
 }
