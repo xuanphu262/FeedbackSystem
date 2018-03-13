1a6a63c2374fd21983b7d811119360b07b8fa7a7
pnguyen <pnguyen@safetrust.com>
2018-03-13 11:27:32 +0700

    - Update data - Change data model - Add more icons

--- FeedbackSystemApp/app/src/main/java/android/mobile/feedbacksystem/common/DataHelper.java
+++ FeedbackSystemApp/app/src/main/java/android/mobile/feedbacksystem/common/DataHelper.java
@@ -3,7 +3,9 @@ package android.mobile.feedbacksystem.common;
 import android.mobile.feedbacksystem.common.model.DeviceModel;
 import android.mobile.feedbacksystem.common.model.LocationModel;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -15,13 +17,21 @@ public class DataHelper {
     public static List<DeviceModel> deviceModelList = new ArrayList<>();
 
     public static void init() {
-        locationList.add(new LocationModel("1", "toilet1", "in H1"));
-        locationList.add(new LocationModel("2", "toilet2", "in H1"));
-        locationList.add(new LocationModel("3", "toilet3", "in H1"));
-        locationList.add(new LocationModel("4", "toilet4", "in H1"));
+
+        // initial data for Location
+        locationList.add(new LocationModel("1", "L1T1", "Toilet", new Date()));
+        locationList.add(new LocationModel("2", "L1T2", "Toilet", new Date()));
+        locationList.add(new LocationModel("3", "L1T3", "Toilet", new Date()));
+        locationList.add(new LocationModel("4", "L1T4", "Toilet", new Date()));
+        locationList.add(new LocationModel("5", "L2T1", "Toilet", new Date()));
+        locationList.add(new LocationModel("6", "L2T2", "Toilet", new Date()));
+        locationList.add(new LocationModel("7", "L1C1", "Counter", new Date()));
+        locationList.add(new LocationModel("8", "L1C2", "Counter", new Date()));
+        locationList.add(new LocationModel("9", "L2C1", "Counter", new Date()));
+        locationList.add(new LocationModel("10", "L2C2", "Counter", new Date()));
 
         for(LocationModel locationModel: locationList) {
-            deviceModelList.add(new DeviceModel(locationModel.getId(), locationModel));
+            deviceModelList.add(new DeviceModel("1", "Tablet " + locationModel.getId() , "9774d56d682e549c", new Date(), locationModel));
         }
     }
 }
