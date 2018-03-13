7c1185e20be423eb1ea0f27c75bcabd183b87869
Hoai Nguyen <hnguyen@safetrust.com>
2018-03-13 19:57:57 +0700

    Update report page.

--- FeedbackSystemApp/app/src/main/java/android/mobile/feedbacksystem/ui/admin/report/ReportFragment.java
+++ FeedbackSystemApp/app/src/main/java/android/mobile/feedbacksystem/ui/admin/report/ReportFragment.java
@@ -1,5 +1,6 @@
 package android.mobile.feedbacksystem.ui.admin.report;
 
+import android.app.DatePickerDialog;
 import android.mobile.feedbacksystem.R;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,7 +9,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by hoainguyen on 3/11/18.
@@ -18,7 +24,9 @@ public class ReportFragment extends Fragment implements View.OnClickListener {
     private TextView mTvStartDay;
     private TextView mTvEndDay;
     private TextView mTvLocation;
-    private TextView mBtnReport;
+    final SimpleDateFormat DateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
+    private Calendar mStartDay = Calendar.getInstance();
+    private Calendar mEndDay = Calendar.getInstance();
 
     @Nullable
     @Override
@@ -27,10 +35,11 @@ public class ReportFragment extends Fragment implements View.OnClickListener {
         mTvStartDay = (TextView) rootView.findViewById(R.id.tv_startDay);
         mTvStartDay.setOnClickListener(this);
         mTvEndDay = (TextView) rootView.findViewById(R.id.tv_endDay);
-        mTvStartDay.setOnClickListener(this);
+        mTvEndDay.setOnClickListener(this);
         mTvLocation = (TextView) rootView.findViewById(R.id.tv_selectLocation);
-        mTvStartDay.setOnClickListener(this);
-        mBtnReport = (TextView) rootView.findViewById(R.id.btn_report);
+        mTvLocation.setOnClickListener(this);
+        rootView.findViewById(R.id.btn_report).setOnClickListener(this);
+
         return rootView;
     }
 
@@ -38,10 +47,37 @@ public class ReportFragment extends Fragment implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.tv_endDay:
+                showDateTimePickerDialog(mEndDay, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mEndDay.set(year, month, dayOfMonth);
+                        mTvEndDay.setText(DateFormat.format(mEndDay.getTime()));
+                    }
+                });
                 break;
             case R.id.tv_startDay:
+                showDateTimePickerDialog(mStartDay, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mStartDay.set(year, month, dayOfMonth);
+                        mTvStartDay.setText(DateFormat.format(mStartDay.getTime()));
+                    }
+                });
                 break;
-
+            case R.id.btn_report:
+                //TODO check input and go to ReportResultActivity
+                break;
+            case R.id.tv_selectLocation:
+                //TODO go to SelectLocationActivity
+                break;
         }
+    }
+
+    private void showDateTimePickerDialog(Calendar currentDay, DatePickerDialog.OnDateSetListener listener) {
+        DatePickerDialog pickerDialog = new DatePickerDialog(getActivity(), listener,
+                currentDay.get(Calendar.YEAR),
+                currentDay.get(Calendar.MONTH),
+                currentDay.get(Calendar.DAY_OF_MONTH));
+        pickerDialog.show();
     }
 }
