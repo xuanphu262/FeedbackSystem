7ca2dc62cfa205330dbcf4498ce8f98685b79398
pnguyen <pnguyen@safetrust.com>
2018-03-14 13:51:29 +0700

    - add function export excel

--- FeedbackSystemApp/app/src/main/java/android/mobile/feedbacksystem/common/AppUtils.java
+++ FeedbackSystemApp/app/src/main/java/android/mobile/feedbacksystem/common/AppUtils.java
@@ -2,7 +2,24 @@ package android.mobile.feedbacksystem.common;
 
 import android.content.Context;
 import android.mobile.feedbacksystem.R;
+import android.os.Environment;
 import android.provider.Settings;
+import android.util.Log;
+
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
 
 public class AppUtils {
     public static int[] FeedbackResource = {
@@ -15,5 +32,66 @@ public class AppUtils {
 
     public static String getDeviceId(Context context) {
         return Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
+    public static boolean saveExcelFile(Context context, String fileName) {
+
+        boolean success = false;
+
+        //New Workbook
+        Workbook wb = new HSSFWorkbook();
+
+        Cell c = null;
+
+        //Cell style for header row
+        CellStyle cs = wb.createCellStyle();
+        cs.setFillForegroundColor(HSSFColor.LIME.index);
+        cs.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+
+        //New Sheet
+        Sheet sheet1 = null;
+        sheet1 = wb.createSheet("report");
+
+        // Generate column headings
+        Row row = sheet1.createRow(0);
+
+        c = row.createCell(0);
+        c.setCellValue("Item Number");
+        c.setCellStyle(cs);
+
+        c = row.createCell(1);
+        c.setCellValue("Rating");
+        c.setCellStyle(cs);
+
+        c = row.createCell(2);
+        c.setCellValue("Location");
+        c.setCellStyle(cs);
+
+        sheet1.setColumnWidth(0, (15 * 500));
+        sheet1.setColumnWidth(1, (15 * 500));
+        sheet1.setColumnWidth(2, (15 * 500));
+
+        // Create a path where we will place our List of objects on external storage
+        File file = new File(Environment.getExternalStorageDirectory(), fileName);
+        FileOutputStream os = null;
+
+        try {
+            os = new FileOutputStream(file);
+            wb.write(os);
+            Log.w("FileUtils", "Writing file" + file);
+            success = true;
+        } catch (IOException e) {
+            Log.w("FileUtils", "Error writing " + file, e);
+        } catch (Exception e) {
+            Log.w("FileUtils", "Failed to save file", e);
+        } finally {
+            try {
+                if (null != os)
+                    os.close();
+            } catch (Exception ex) {
+            }
+        }
+
+        return success;
     }
 }
--- FeedbackSystemApp/app/src/main/java/android/mobile/feedbacksystem/ui/admin/result/ListResultFragment.java
+++ FeedbackSystemApp/app/src/main/java/android/mobile/feedbacksystem/ui/admin/result/ListResultFragment.java
@@ -1,12 +1,32 @@
 package android.mobile.feedbacksystem.ui.admin.result;
 
+import android.mobile.feedbacksystem.R;
+import android.mobile.feedbacksystem.common.AppUtils;
+import android.mobile.feedbacksystem.ui.admin.event.NavigateEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.greenrobot.eventbus.EventBus;
+
 import hoainguyen.lib.recyclerhelper.collection.SectionCollectionFragment;
 
-/**
- * Created by hoainguyen on 3/13/18.
- */
-
 public class ListResultFragment extends SectionCollectionFragment {
+
+    @Override
+    public View getRootLayout(LayoutInflater inflater, ViewGroup container) {
+        View rootView = inflater.inflate(R.layout.fragment_list_result, null);
+        rootView.findViewById(R.id.btn_export).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               boolean result =  AppUtils.saveExcelFile(getActivity().getApplicationContext(), "myExcel.xlsx");
+
+
+            }
+        });
+        return rootView;
+    }
+
     @Override
     protected void onMakeAdapters() {
         mRecyclerView.appendAdapter(new FeedbackAdapter());
